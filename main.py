9ebdb5b52c34680b8604e9201eb5d9a0f608399f
Oleksandr Semenenko <oleksandr.semenenko@icloud.com>
2025-03-12 11:29:01 +0100

    web elements commands added

--- main.py
+++ main.py
@@ -13,19 +13,24 @@ time.sleep(2)
 
 # Type username student into Username field
 username_locator = driver.find_element(By.ID, "username")
-
+username_locator.send_keys("student")
 
 # Type password Password123 into Password field
 password_locator = driver.find_element(By.NAME, "password")
+password_locator.send_keys("Password123")
 
 # Push Submit button
 submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
-
+submit_button_locator.click()
+time.sleep(2)
 
 # Verify new page URL contains practicetestautomation.com/logged-in-successfully/
+actual_url = driver.current_url
 
 # Verify new page contains expected text ('Congratulations' or 'successfully logged in')
 text_locator = driver.find_element(By.TAG_NAME, "h1")
+actual_text = text_locator.text
 
 # Verify button Log out is displayed on the new page
 logout_button_locator = driver.find_element((By.LINK_TEXT, "Log out"))
+logout_button_locator.click()
