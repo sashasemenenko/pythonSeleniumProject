efa6c48b7db15dd954f3a93da9ea87f9a6b0e6f3
Oleksandr Semenenko <oleksandr.semenenko@icloud.com>
2025-03-26 11:10:25 +0100

    custom command line parameter

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+# from pydoc import browse
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
@@ -5,10 +7,21 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 
 @pytest.fixture()
-def driver():
-    print("Creating chrome driver")
-    # my_driver = webdriver.Chrome()
-    my_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+def driver(request):
+    browser = request.config.getoption("--browser")
+    print(f"Creating {browser} driver")
+    if browser == "chrome":
+        my_driver = webdriver.Chrome()
+    elif browser == "firefox":
+        my_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    else:
+        raise TypeError(f"Expected 'chrome' or 'firefox' but got {browser}")
     yield my_driver
-    print("Closing chrome driver")
+    print(f"Closing {browser} driver")
     my_driver.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="chrome", help="browser to execute tests (chrome or firefox"
+    )
